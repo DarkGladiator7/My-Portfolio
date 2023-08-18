df23d65211ca58966b63acca447ed2ab21c5b5a3
DarkGladiator7 <ganeshkrishnadoss@gmail.com>
2023-08-18 22:18:02 +0530

    created a minimalistic nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,26 @@
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
+import {
+  TwitterIcon,
+  DribbbleIcon,
+  GithubIcon,
+  LinkedInIcon,
+  PinterestIcon,
+} from "./Icons";
+import { motion } from "framer-motion";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
   return (
     <Link href={href} className={`${className} relative group`}>
       {title}
-      <span className="h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300">
+      <span
+        className={`h-[1px] inline-block  bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+      ${router.asPath === href ? "w-full" : "w-0"}`}
+      >
         &nbsp;
       </span>
     </Link>
@@ -23,25 +37,52 @@ const NavBar = () => {
         <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
 
-      <nav>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
-        <Link href="/" target={"_blank"}>
-          T
-        </Link>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://twitter.com/DGladiator7"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          href="https://github.com/DarkGladiator7"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/ganeshkrishnadoss/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="https://in.pinterest.com/ganeshkrishnadoss/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <PinterestIcon />
+        </motion.a>
+        <motion.a
+          href="https://dribbble.com/darkgladiator7"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <DribbbleIcon />
+        </motion.a>
       </nav>
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
