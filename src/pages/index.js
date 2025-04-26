ed0cd23acee824029d45b2c7840d9450a89b236e
DarkGladiator7 <ganeshkrishnadoss@gmail.com>
2025-04-26 13:05:48 +0530

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,14 +50,14 @@ export default function Home() {
               </p>
               <div className=" flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/Resume3.pdf"
+                  href="/GaneshK_CV.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg md:p-2 md:px-4 md:text-base sm:text-sm
                    dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
                   download={true}
                 >
-                  Resume
-                  <LinkArrow className={"w-6 ml-1 sm:!w-5"} />
+                  Curriculum Vitae
+                  <LinkArrow className={"w-5 ml-1 sm:!w-5"} />
                 </Link>
                 <Link
                   href="mailto:ganeshkrishnadoss@gmail.com"
