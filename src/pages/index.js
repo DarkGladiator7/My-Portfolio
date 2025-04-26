b6d622b2da01e622e35bf285dd0c36410e7d5443
DarkGladiator7 <ganeshkrishnadoss@gmail.com>
2025-04-26 13:28:05 +0530

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
               </p>
               <div className=" flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/GaneshK_CV1.pdf"
+                  href="/GaneshK_CV.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg md:p-2 md:px-4 md:text-base sm:text-sm
                    dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
